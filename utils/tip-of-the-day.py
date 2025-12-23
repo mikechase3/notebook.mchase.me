86085d0fa3b145346fc7fd09427ce1490c6eebf9
Mike Chase <protectmikechase@gmail.com>
2025-12-23 02:19:28 -0500

    prototyped a little tool to quiz

---
+++ utils/tip-of-the-day.py
@@ -0,0 +1,13 @@
+# (C) DAM-IT LLC / Written by Mike Chase
+# A program to remember things.
+# Make sure uv or pip3 `pandas` into your system's python3 env
+
+# TODO: More visualizing
+# import tkinter as tk
+# import pandas as pd
+from typing import list, dict
+
+lessons_learned = {
+        ("How do you install a python package system wide?", "Use apt-get install python3-<nameOfPackage>"),
+        ("How do you start syncthing to sync picture library?", "syncthing serve, fg, suspend")
+        }
